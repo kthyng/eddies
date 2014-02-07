bf4c8e359f8cf2eb6c3280c0327cf04b809b0a79
Kristen M. Thyng <kthyng@gmail.com>
2014-02-07 16:09:10 -0600

    salinity plot is good, working on drifter overlay

--- make_plots.py
+++ make_plots.py
@@ -13,6 +13,8 @@ import tracpy
 import init
 from datetime import datetime, timedelta
 import glob
+import cm_pong
+from matplotlib.mlab import find
 
 # mpl.rcParams['text.usetex'] = True
 mpl.rcParams.update({'font.size': 26})
@@ -28,7 +30,8 @@ mpl.rcParams['mathtext.fallback_to_cm'] = 'True'
 
 # Grid info
 loc = 'http://barataria.tamu.edu:8080/thredds/dodsC/NcML/txla_nesting6.nc'
-grid = tracpy.inout.readgrid(loc, llcrnrlat=27, urcrnrlat=30.5, llcrnrlon=-98)
+grid = tracpy.inout.readgrid(loc, llcrnrlat=27.01, 
+        urcrnrlat=30.5, llcrnrlon=-97.8, urcrnrlon=-87.7)
 xr = np.asanyarray(grid['xr'].T, order='C')
 yr = np.asanyarray(grid['yr'].T, order='C')
 
@@ -51,102 +54,88 @@ Files = glob.glob('tracks/2007' + '*gc.nc')
 ##
 
 ## Model output ##
-d = netCDF.Dataset(loc)
+m = netCDF.Dataset(loc)
 
 # Model time period to use
-starttime = netCDF.date2num(datetime(2007, 5, 1, 12, 0, 0), d.variables['ocean_time'].units)
-endtime = netCDF.date2num(datetime(2007, 9, 1, 12, 0, 0), d.variables['ocean_time'].units)
-dt = d.variables['ocean_time'][1] - d.variables['ocean_time'][0] # 4 hours in seconds
+starttime = netCDF.date2num(datetime(2007, 5, 1, 12, 0, 0), m.variables['ocean_time'].units)
+endtime = netCDF.date2num(datetime(2007, 9, 1, 12, 0, 0), m.variables['ocean_time'].units)
+dt = m.variables['ocean_time'][1] - m.variables['ocean_time'][0] # 4 hours in seconds
 ts = np.arange(starttime, endtime, dt)
-itshift = find(starttime==d.variables['ocean_time'][:]) # shift to get to the right place in model output
+itshift = find(starttime==m.variables['ocean_time'][:]) # shift to get to the right place in model output
+dates = netCDF.num2date(m.variables['ocean_time'][:], m.variables['ocean_time'].units)
+months = ['January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December']
+
+# Colormap for model output
+levels = (37-np.exp(linspace(0,np.log(36.), 10)))[::-1]-1 # log for salinity
+cmap = cm_pong.salinity('YlGnBu', levels)
+ilevels = [0,1,2,3,4,5,8] # which levels to label
+ticks = [int(tick) for tick in levels[ilevels]] # plot ticks
 ##
 
-## Colormap ##
-cdict = {'red': ((0.0, 0.0, 0.0),
-                 (0.4, 1.0, 0.7),
-                 (1.0, 1.0, 1.0)),
-         'green': ((0.0, 0.0, 0.0),
-                   (0.4, 1.0, 0.0),
-                   (1.0, 1.0, 1.0)),
-         'blue': ((0.0, 0.0, 0.0),
-                  (0.4, 1.0, 0.0),
-                  (1.0, 0.5, 1.0))}
-my_cmap = matplotlib.colors.LinearSegmentedColormap('my_colormap',cdict,256)
-pcolor(rand(10,10),cmap=my_cmap)
-colorbar()
-
-np.array([0, 13, 21, 26, 30, 32, 34, 35, 35.5, 36])
-
-cdict = {'red': ((0/36., 0.0, 0.1),
-                 (13/36., 0.1, 0.2),
-                 (21/36., 0.2, 0.3),
-                 (26/36., 0.3, 0.4),
-                 (30/36., 0.4, 0.5),
-                 (32/36., 0.5, 0.6),
-                 (34/36., 0.6, 0.7),
-                 (35/36., 0.7, 0.8),
-                 (35.5/36., 0.8, 0.9),
-                 (36/36., 0.9, 1.0)),
-		'green': ((0/36., 0.0, 0.1),
-                 (13/36., 0.1, 0.2),
-                 (21/36., 0.2, 0.3),
-                 (26/36., 0.3, 0.4),
-                 (30/36., 0.4, 0.5),
-                 (32/36., 0.5, 0.6),
-                 (34/36., 0.6, 0.7),
-                 (35/36., 0.7, 0.8),
-                 (35.5/36., 0.8, 0.9),
-                 (36/36., 0.9, 1.0)),
-		'blue': ((0/36., 1, 1),
-                 (13/36., 1, 1),
-                 (21/36., 1, 1),
-                 (26/36., 1, 1),
-                 (30/36., 1, 1),
-                 (32/36., 1, 1),
-                 (34/36., 1, 1),
-                 (35/36., 1, 1),
-                 (35.5/36., 1, 1),
-                 (36/36., 1, 1.0))}
+    # # Change axis and label color
+    # ax.spines['bottom'].set_color('0.2')
+    # ax.spines['top'].set_color('0.2')
+    # ax.spines['left'].set_color('0.2')
+    # ax.spines['right'].set_color('0.2')
+    # ax.xaxis.label.set_color('0.2')
+    # ax.yaxis.label.set_color('0.2')
+    # ax.tick_params(axis='x', colors='0.2')
+    # ax.tick_params(axis='y', colors='0.2')
 
 
-# Loop through time
-for it,t in enumerate(ts):
+# Loop through times that simulations were started
+for t in ts[5:6]:
 
-	# Set up plot
-fig = plt.figure(figsize=(20,10))
-ax = fig.add_subplot(111)
-tracpy.plotting.background(grid=grid, ax=ax)
+    # Set up plot
+    fig = plt.figure(figsize=(17,9))
+    ax = fig.add_subplot(111)
+    tracpy.plotting.background(grid=grid, ax=ax, outline=False)
 
-# Plot surface salinity
-salt = np.squeeze(d.variables['salt'][it+itshift,-1,:,:])
-plt.contourf(xr, yr, salt, levels=np.array([ 1.        ,  12.82424751,  20.76480532,  26.09727644,
-        29.67829038,  32.08311557,  33.69807275,  34.78259511,
-        35.51090468,  36.]),
-			cmap='Blues')#, vmin=5, vmax=35)
+    itmodel = find(t==m.variables['ocean_time'][:])[0]
 
-# Colorbar in upper left corner
-cax = fig.add_axes([0.15, 0.75, 0.3, 0.03]) #colorbar axes
-cb = colorbar(cax=cax, orientation='horizontal')
-cb.set_label('Surface salinity [g$\cdot$kg$^{-1}$]', fontsize=20)
-cb.ax.tick_params(labelsize=20) 
+    # # Plot surface salinity
+    # salt = np.squeeze(m.variables['salt'][itmodel,-1,:,:])
+    # ax.contour(xr, yr, salt, [33], colors='k')
+    # mappable = ax.pcolormesh(xr, yr, salt, cmap=cmap, vmin=0, vmax=35)
 
-	# Read in all drifter sets starting before or up to the current datetime
-	for File for Files:
-		d = netCDF.Dataset(File)
-		xg = d.variables['xg'][:]
-		yg = d.variables['yg'][:]
-		tg = d.variables['tp'][:]
-		d.close()
+    # # Date
+    # date = str(dates[itmodel].year) + ' ' + months[dates[itmodel].month-1] \
+    #      + ' ' + str(dates[itmodel].day) + ' ' + str(dates[itmodel].hour).fill(2) + ':00'
+    # text(0.75, 0.02, date, 
+    #         fontsize=24, color='0.2', transform=ax.transAxes)
 
-		# Change to projected drifter locations now
-		nanind = np.isnan(xg) # indices where nans are location in xg, yg; for reinstitution of nans
-		xp, yp, _ = tracpy.tools.interpolate2d(xg, yg, grid, 'm_ij2xy') 
-		xp[nanind] = np.nan; yp[nanind] = np.nan
-		del(xg,yg) # don't need grid info anymore
+    # # Colorbar in upper left corner
+    # cax = fig.add_axes([0.15, 0.75, 0.3, 0.03]) #colorbar axes
+    # cb = colorbar(mappable, cax=cax, orientation='horizontal')
+    # cb.set_label('Surface salinity [g$\cdot$kg$^{-1}$]', fontsize=20)
+    # cb.ax.tick_params(labelsize=18) 
+    # cb.set_ticks(ticks)
 
-	# Plot drifters
-	plot(xp[:,tind], yp[:,tind], 'o', color='darkcyan')
+    # Read in all drifter sets starting before or up to the current datetime
+    for j,File in enumerate(Files[0:1]):
 
-	# ax.plot(xp[::5,tind], yp[::5,tind], 'o', color='darkcyan')
-	# plt.savefig('figures/2010-07-01T00/' + str(tind) + '.png', dpi=150)
-	# plt.close(fig)
+        d = netCDF.Dataset(File)
+        xg = d.variables['xg'][:]
+        yg = d.variables['yg'][:]
+        tg = d.variables['tp'][:]
+        d.close()
+
+        if tg[0]>t: # this drifter simulation starts after this time
+            continue
+        else:
+            itdrifter = find(tg==t)[0] # find index in time for drifters
+
+        # Change to projected drifter locations now
+        nanind = np.isnan(xg) # indices where nans are location in xg, yg; for reinstitution of nans
+        xp, yp, _ = tracpy.tools.interpolate2d(xg, yg, grid, 'm_ij2xy') 
+        xp[nanind] = np.nan; yp[nanind] = np.nan
+        # del(xg,yg) # don't need grid info anymore
+
+        # Plot drifters
+        ax.plot(xp[:,:itdrifter].T, yp[:,:itdrifter].T, '-', color='0.5', zorder=7)
+        ax.plot(xp[:,itdrifter], yp[:,itdrifter], 'o', color='0.5', zorder=7)
+
+    # ax.plot(xp[::5,tind], yp[::5,tind], 'o', color='darkcyan')
+    # plt.savefig('figures/2010-07-01T00/' + str(tind) + '.png', dpi=150)
+    # plt.close(fig)
