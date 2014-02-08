586b96402fc95b606ca5a2fc101505cfc25fb359
Kristen M. Thyng <kthyng@gmail.com>
2014-02-07 19:12:58 -0600

    drifter-over-salinity-field plots are at a good palce

--- make_plots.py
+++ make_plots.py
@@ -35,34 +35,18 @@ grid = tracpy.inout.readgrid(loc, llcrnrlat=27.01,
 xr = np.asanyarray(grid['xr'].T, order='C')
 yr = np.asanyarray(grid['yr'].T, order='C')
 
-## Drifters ##
-# Tracks to plot
-Files = glob.glob('tracks/2007' + '*gc.nc')
-
-# # Read in info
-# d = netCDF.MFDataset(Files, aggdim='ntrac')
-# xg = d.variables['xg'][:]
-# yg = d.variables['yg'][:]
-# tg = d.variables['tp'][:]
-# d.close()
-
-# # Change to projected drifter locations now
-# nanind = np.isnan(xg) # indices where nans are location in xg, yg; for reinstitution of nans
-# xp, yp, _ = tracpy.tools.interpolate2d(xg, yg, grid, 'm_ij2xy') 
-# xp[nanind] = np.nan; yp[nanind] = np.nan
-# del(xg,yg) # don't need grid info anymore
-##
-
 ## Model output ##
 m = netCDF.Dataset(loc)
 
 # Model time period to use
-starttime = netCDF.date2num(datetime(2007, 5, 1, 12, 0, 0), m.variables['ocean_time'].units)
-endtime = netCDF.date2num(datetime(2007, 9, 1, 12, 0, 0), m.variables['ocean_time'].units)
+units = m.variables['ocean_time'].units
+year = 2007
+starttime = netCDF.date2num(datetime(year, 5, 1, 12, 0, 0), units)
+endtime = netCDF.date2num(datetime(year, 9, 1, 12, 0, 0), units)
 dt = m.variables['ocean_time'][1] - m.variables['ocean_time'][0] # 4 hours in seconds
 ts = np.arange(starttime, endtime, dt)
 itshift = find(starttime==m.variables['ocean_time'][:]) # shift to get to the right place in model output
-dates = netCDF.num2date(m.variables['ocean_time'][:], m.variables['ocean_time'].units)
+dates = netCDF.num2date(m.variables['ocean_time'][:], units)
 months = ['January', 'February', 'March', 'April', 'May', 'June',
     'July', 'August', 'September', 'October', 'November', 'December']
 
@@ -85,35 +69,58 @@ ticks = [int(tick) for tick in levels[ilevels]] # plot ticks
 
 
 # Loop through times that simulations were started
-for t in ts[5:6]:
+for t in ts[:10]:
+
+    # Set up before plotting
+
+    x14 = []; y14 = []; x7 = []; y7 = []; x = []; y = []
+
+    itmodel = find(t==m.variables['ocean_time'][:])[0] # index for model output at this time
+
+    ## Drifter file set up ##
+    # Read in all drifter sets starting before or up to the current datetime
+    lastsimdate = netCDF.num2date(t, units) # start date we're examining
+    # Read in drifters from simulations that would reach this date; they run for 90 days
+    firstsimdate = (netCDF.num2date(t, units)-timedelta(days=90))
+    if firstsimdate < datetime(year, 5, 1, 12, 0, 0):
+        firstsimdate = datetime(year, 5, 1, 12, 0, 0) # earliest simulation
+    nsims = 1 + (lastsimdate-firstsimdate).total_seconds()/(3600*4.) # number of sims between start and end
+    simdates = [firstsimdate + i*timedelta(hours=4) for i in xrange(int(nsims))]
+    Files = []
+    for i,simdate in enumerate(simdates):
+        Files.append('tracks/' + simdates[i].isoformat()[0:13] + 'gc.nc')
+    ##
+
+    figname = 'figures/' + str(year) + '/' + lastsimdate.isoformat()[0:13] + '.png'
+
+    # Don't redo plot
+    if os.path.exists(figname):
+        continue
 
     # Set up plot
     fig = plt.figure(figsize=(17,9))
     ax = fig.add_subplot(111)
     tracpy.plotting.background(grid=grid, ax=ax, outline=False)
 
-    itmodel = find(t==m.variables['ocean_time'][:])[0]
+    # Date
+    date = str(dates[itmodel].year) + ' ' + months[dates[itmodel].month-1] \
+         + ' ' + str(dates[itmodel].day) + ' ' + str(dates[itmodel].hour).zfill(2) + ':00'
+    text(0.75, 0.02, date, fontsize=24, color='0.2', transform=ax.transAxes)
 
-    # # Plot surface salinity
-    # salt = np.squeeze(m.variables['salt'][itmodel,-1,:,:])
+    # Plot surface salinity
+    salt = np.squeeze(m.variables['salt'][itmodel,-1,:,:])
     # ax.contour(xr, yr, salt, [33], colors='k')
-    # mappable = ax.pcolormesh(xr, yr, salt, cmap=cmap, vmin=0, vmax=35)
+    mappable = ax.pcolormesh(xr, yr, salt, cmap=cmap, vmin=0, vmax=35)
 
-    # # Date
-    # date = str(dates[itmodel].year) + ' ' + months[dates[itmodel].month-1] \
-    #      + ' ' + str(dates[itmodel].day) + ' ' + str(dates[itmodel].hour).fill(2) + ':00'
-    # text(0.75, 0.02, date, 
-    #         fontsize=24, color='0.2', transform=ax.transAxes)
+    # Colorbar in upper left corner
+    cax = fig.add_axes([0.15, 0.75, 0.3, 0.03]) #colorbar axes
+    cb = colorbar(mappable, cax=cax, orientation='horizontal')
+    cb.set_label('Surface salinity [g$\cdot$kg$^{-1}$]', fontsize=20)
+    cb.ax.tick_params(labelsize=18) 
+    cb.set_ticks(ticks)
 
-    # # Colorbar in upper left corner
-    # cax = fig.add_axes([0.15, 0.75, 0.3, 0.03]) #colorbar axes
-    # cb = colorbar(mappable, cax=cax, orientation='horizontal')
-    # cb.set_label('Surface salinity [g$\cdot$kg$^{-1}$]', fontsize=20)
-    # cb.ax.tick_params(labelsize=18) 
-    # cb.set_ticks(ticks)
-
-    # Read in all drifter sets starting before or up to the current datetime
-    for j,File in enumerate(Files[0:1]):
+    # I think I need to loop to use the right amount of time from each set of drifters
+    for j,File in enumerate(Files):
 
         d = netCDF.Dataset(File)
         xg = d.variables['xg'][:]
@@ -121,21 +128,37 @@ for t in ts[5:6]:
         tg = d.variables['tp'][:]
         d.close()
 
-        if tg[0]>t: # this drifter simulation starts after this time
-            continue
-        else:
-            itdrifter = find(tg==t)[0] # find index in time for drifters
+        itdrifter = find(tg==t)[0] # find index in drifter time for drifters
+
+        # Days back from this time
+        days = (tg-tg[0])/(3600.*24)
 
         # Change to projected drifter locations now
-        nanind = np.isnan(xg) # indices where nans are location in xg, yg; for reinstitution of nans
+        nanind = np.isnan(xg)*(xg==-1) # indices where nans are location in xg, yg; for reinstitution of nans
         xp, yp, _ = tracpy.tools.interpolate2d(xg, yg, grid, 'm_ij2xy') 
         xp[nanind] = np.nan; yp[nanind] = np.nan
-        # del(xg,yg) # don't need grid info anymore
+        del(xg,yg) # don't need grid info anymore
 
-        # Plot drifters
-        ax.plot(xp[:,:itdrifter].T, yp[:,:itdrifter].T, '-', color='0.5', zorder=7)
-        ax.plot(xp[:,itdrifter], yp[:,itdrifter], 'o', color='0.5', zorder=7)
+        # Plot drifter tails for 7 days one color, 14 days another lighter color (find indices)
+        # these are 0 if enough time hasn't passed, so works either way
+        i7 = find(days>days[itdrifter]-7)[0]
+        i14 = find(days>days[itdrifter]-14)[0]
 
-    # ax.plot(xp[::5,tind], yp[::5,tind], 'o', color='darkcyan')
-    # plt.savefig('figures/2010-07-01T00/' + str(tind) + '.png', dpi=150)
-    # plt.close(fig)
+        # Save drifter info and plot all at once so things don't overlap
+        x14.append(xp[:,i14:itdrifter+1].T)
+        y14.append(yp[:,i14:itdrifter+1].T)
+        x7.append(xp[:,i7:itdrifter+1].T)
+        y7.append(yp[:,i7:itdrifter+1].T)
+        x.append(xp[:,itdrifter])
+        y.append(yp[:,itdrifter])
+
+    # Plot drifters
+    for i in xrange(len(x14)):
+        ax.plot(x14[i], y14[i], '-', color='0.7', zorder=7, linewidth=1, alpha=0.5)
+    for i in xrange(len(x14)):
+        ax.plot(x7[i], y7[i], '-', color='0.5', zorder=7, linewidth=2, alpha=0.7)
+    for i in xrange(len(x14)):
+        ax.plot(x[i], y[i], 'o', color='0.3', zorder=7)
+
+    plt.savefig(figname, bbox_inches='tight', dpi=150)
+    plt.close(fig)
